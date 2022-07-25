8d5a0b537105bf3e782ee440b9f9fa00b93ac7bd
Anthony <anthony@unfinishedteleporter.com>
2022-07-25 15:55:11 +0800

    [LIST-286] civix upgrade

--- civiauth0jwt.civix.php
+++ civiauth0jwt.civix.php
@@ -6,7 +6,7 @@
  * The ExtensionUtil class provides small stubs for accessing resources of this
  * extension.
  */
-class CRM_CiviAuth0jwt_ExtensionUtil {
+class CRM_CiviAuth0Jwt_ExtensionUtil {
   const SHORT_NAME = 'civiauth0jwt';
   const LONG_NAME = 'civiauth0jwt';
   const CLASS_PREFIX = 'CRM_CiviAuth0Jwt';
@@ -84,15 +84,17 @@ use CRM_CiviAuth0Jwt_ExtensionUtil as E;
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config
  */
 function _civiauth0jwt_civix_civicrm_config(&$config = NULL) {
+  \CRM_Core_Error::debug_log_message("_civiauth0jwt_civix_civicrm_config()");
+
   static $configured = FALSE;
   if ($configured) {
     return;
   }
   $configured = TRUE;
 
-  $template = &CRM_Core_Smarty::singleton();
+  $template = CRM_Core_Smarty::singleton();
 
-  $extRoot = dirname(__FILE__) . DIRECTORY_SEPARATOR;
+  $extRoot = __DIR__ . DIRECTORY_SEPARATOR;
   $extDir = $extRoot . 'templates';
 
   if (is_array($template->template_dir)) {
@@ -103,19 +105,7 @@ function _civiauth0jwt_civix_civicrm_config(&$config = NULL) {
 
   $include_path = $extRoot . PATH_SEPARATOR . get_include_path();
   set_include_path($include_path);
-}
-
-/**
- * (Delegated) Implements hook_civicrm_xmlMenu().
- *
- * @param $files array(string)
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function _civiauth0jwt_civix_civicrm_xmlMenu(&$files) {
-  foreach (_civiauth0jwt_civix_glob(__DIR__ . '/xml/Menu/*.xml') as $file) {
-    $files[] = $file;
-  }
+  \CRM_Core_Error::debug_log_message("_civiauth0jwt_civix_civicrm_config path updated(): added $extRoot");
 }
 
 /**
@@ -204,144 +194,14 @@ function _civiauth0jwt_civix_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL)
 }
 
 /**
- * @return CRM_Auth0jwt_Upgrader
+ * @return CRM_CiviAuth0Jwt_Upgrader
  */
 function _civiauth0jwt_civix_upgrader() {
-  if (!file_exists(__DIR__ . '/CRM/Auth0jwt/Upgrader.php')) {
+  if (!file_exists(__DIR__ . '/CRM/CiviAuth0Jwt/Upgrader.php')) {
     return NULL;
   } else {
-    return CRM_Auth0jwt_Upgrader_Base::instance();
+    return CRM_CiviAuth0Jwt_Upgrader_Base::instance();
   }
-}
-
-/**
- * Search directory tree for files which match a glob pattern.
- *
- * Note: Dot-directories (like "..", ".git", or ".svn") will be ignored.
- * Note: Delegate to CRM_Utils_File::findFiles(), this function kept only
- * for backward compatibility of extension code that uses it.
- *
- * @param string $dir base dir
- * @param string $pattern , glob pattern, eg "*.txt"
- *
- * @return array
- */
-function _civiauth0jwt_civix_find_files($dir, $pattern) {
-  return CRM_Utils_File::findFiles($dir, $pattern);
-}
-
-/**
- * (Delegated) Implements hook_civicrm_managed().
- *
- * Find any *.mgd.php files, merge their content, and return.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
- */
-function _civiauth0jwt_civix_civicrm_managed(&$entities) {
-  $mgdFiles = _civiauth0jwt_civix_find_files(__DIR__, '*.mgd.php');
-  sort($mgdFiles);
-  foreach ($mgdFiles as $file) {
-    $es = include $file;
-    foreach ($es as $e) {
-      if (empty($e['module'])) {
-        $e['module'] = E::LONG_NAME;
-      }
-      if (empty($e['params']['version'])) {
-        $e['params']['version'] = '3';
-      }
-      $entities[] = $e;
-    }
-  }
-}
-
-/**
- * (Delegated) Implements hook_civicrm_caseTypes().
- *
- * Find any and return any files matching "xml/case/*.xml"
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function _civiauth0jwt_civix_civicrm_caseTypes(&$caseTypes) {
-  if (!is_dir(__DIR__ . '/xml/case')) {
-    return;
-  }
-
-  foreach (_civiauth0jwt_civix_glob(__DIR__ . '/xml/case/*.xml') as $file) {
-    $name = preg_replace('/\.xml$/', '', basename($file));
-    if ($name != CRM_Case_XMLProcessor::mungeCaseType($name)) {
-      $errorMessage = sprintf("Case-type file name is malformed (%s vs %s)", $name, CRM_Case_XMLProcessor::mungeCaseType($name));
-      throw new CRM_Core_Exception($errorMessage);
-    }
-    $caseTypes[$name] = [
-      'module' => E::LONG_NAME,
-      'name' => $name,
-      'file' => $file,
-    ];
-  }
-}
-
-/**
- * (Delegated) Implements hook_civicrm_angularModules().
- *
- * Find any and return any files matching "ang/*.ang.php"
- *
- * Note: This hook only runs in CiviCRM 4.5+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function _civiauth0jwt_civix_civicrm_angularModules(&$angularModules) {
-  if (!is_dir(__DIR__ . '/ang')) {
-    return;
-  }
-
-  $files = _civiauth0jwt_civix_glob(__DIR__ . '/ang/*.ang.php');
-  foreach ($files as $file) {
-    $name = preg_replace(':\.ang\.php$:', '', basename($file));
-    $module = include $file;
-    if (empty($module['ext'])) {
-      $module['ext'] = E::LONG_NAME;
-    }
-    $angularModules[$name] = $module;
-  }
-}
-
-/**
- * (Delegated) Implements hook_civicrm_themes().
- *
- * Find any and return any files matching "*.theme.php"
- */
-function _civiauth0jwt_civix_civicrm_themes(&$themes) {
-  $files = _civiauth0jwt_civix_glob(__DIR__ . '/*.theme.php');
-  foreach ($files as $file) {
-    $themeMeta = include $file;
-    if (empty($themeMeta['name'])) {
-      $themeMeta['name'] = preg_replace(':\.theme\.php$:', '', basename($file));
-    }
-    if (empty($themeMeta['ext'])) {
-      $themeMeta['ext'] = E::LONG_NAME;
-    }
-    $themes[$themeMeta['name']] = $themeMeta;
-  }
-}
-
-/**
- * Glob wrapper which is guaranteed to return an array.
- *
- * The documentation for glob() says, "On some systems it is impossible to
- * distinguish between empty match and an error." Anecdotally, the return
- * result for an empty match is sometimes array() and sometimes FALSE.
- * This wrapper provides consistency.
- *
- * @link http://php.net/glob
- * @param string $pattern
- *
- * @return array
- */
-function _civiauth0jwt_civix_glob($pattern) {
-  $result = glob($pattern);
-  return is_array($result) ? $result : [];
 }
 
 /**
@@ -422,18 +282,6 @@ function _civiauth0jwt_civix_fixNavigationMenuItems(&$nodes, &$maxNavID, $parent
     if (isset($nodes[$origKey]['child']) && is_array($nodes[$origKey]['child'])) {
       _civiauth0jwt_civix_fixNavigationMenuItems($nodes[$origKey]['child'], $maxNavID, $nodes[$origKey]['attributes']['navID']);
     }
-  }
-}
-
-/**
- * (Delegated) Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function _civiauth0jwt_civix_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  $settingsDir = __DIR__ . DIRECTORY_SEPARATOR . 'settings';
-  if (!in_array($settingsDir, $metaDataFolders) && is_dir($settingsDir)) {
-    $metaDataFolders[] = $settingsDir;
   }
 }
 
--- civiauth0jwt.php
+++ civiauth0jwt.php
@@ -34,7 +34,6 @@ function civiauth0jwt_civicrm_crypto($registry) {
   }
 }
 
-
 /**
  * Implement hook_civicrm_container() to add our subscriber to listen for authx
  * credential events.
@@ -45,17 +44,6 @@ function civiauth0jwt_civicrm_container(\Symfony\Component\DependencyInjection\C
   $container->register('civiauth0jwt_credentials', '\Civi\CiviAuth0Jwt\CheckAuth0JwtCredential')
     ->addTag('kernel.event_subscriber')
     ->setPublic(TRUE);
-}
-
-
-
-/**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function civiauth0jwt_civicrm_xmlMenu(&$files) {
-  _civiauth0jwt_civix_civicrm_xmlMenu($files);
 }
 
 /**
@@ -113,54 +101,6 @@ function civiauth0jwt_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
 }
 
 /**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
- */
-function civiauth0jwt_civicrm_managed(&$entities) {
-  _civiauth0jwt_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function civiauth0jwt_civicrm_caseTypes(&$caseTypes) {
-  _civiauth0jwt_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function civiauth0jwt_civicrm_angularModules(&$angularModules) {
-  _civiauth0jwt_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function civiauth0jwt_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _civiauth0jwt_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
  * Implements hook_civicrm_entityTypes().
  *
  * Declare entity types provided by this module.
@@ -169,13 +109,6 @@ function civiauth0jwt_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
  */
 function civiauth0jwt_civicrm_entityTypes(&$entityTypes) {
   _civiauth0jwt_civix_civicrm_entityTypes($entityTypes);
-}
-
-/**
- * Implements hook_civicrm_themes().
- */
-function civiauth0jwt_civicrm_themes(&$themes) {
-  _civiauth0jwt_civix_civicrm_themes($themes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
