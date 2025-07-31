ed032ee2af15b610c8216128f08b6b2785896695
John Twyman <john.twyman@greens.org.au>
2025-07-31 14:19:31 +1000

    Run civix upgrade

--- civiauth0jwt.php
+++ civiauth0jwt.php
@@ -66,59 +66,12 @@ function civiauth0jwt_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function civiauth0jwt_civicrm_postInstall() {
-  _civiauth0jwt_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function civiauth0jwt_civicrm_uninstall() {
-  _civiauth0jwt_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function civiauth0jwt_civicrm_enable() {
   _civiauth0jwt_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function civiauth0jwt_civicrm_disable() {
-  _civiauth0jwt_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function civiauth0jwt_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _civiauth0jwt_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function civiauth0jwt_civicrm_entityTypes(&$entityTypes) {
-  _civiauth0jwt_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
